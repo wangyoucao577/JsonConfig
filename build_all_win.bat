43355a02316a9ec220290f69fbefe5fbe2c2390b
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-10 23:31:35 +0800

    新增 vs14_release_x64 的项目配置。目前可正常编译，但运行还有问题。

--- build_all_win.bat
+++ build_all_win.bat
@@ -7,13 +7,13 @@ IF NOT %ERRORLEVEL% EQU 0 (
 echo "ERROR!!! Build failed here!" 
 goto end)
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" projects\json_config_vs14.sln /rebuild "Debug|x86" 
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" projects\json_config_vs14.sln /rebuild "Release|x64" 
 echo %ERRORLEVEL%
 IF NOT %ERRORLEVEL% EQU 0 (
 echo "ERROR!!! Build failed here!" 
 goto end)
 
-echo "Make all succeed!"
+echo "Build all succeed!"
 
 :end
 timeout 30
