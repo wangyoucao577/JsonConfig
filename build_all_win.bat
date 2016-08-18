cc59faccd9b85c43bc30c0ed741185a08c0a21c1
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-18 10:19:18 +0800

    build_all脚本中的exe路径引用。

--- build_all_win.bat
+++ build_all_win.bat
@@ -1,14 +1,15 @@
+@echo off
 
+set VS_14_EXE_PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
 
-
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" projects\json_config_vs14.sln /rebuild "Debug|x64" 
-echo %ERRORLEVEL%
+%VS_14_EXE_PATH% projects\json_config_vs14.sln /rebuild "Debug|x64" 
+echo "ERRORLEVEL:"%ERRORLEVEL%
 IF NOT %ERRORLEVEL% EQU 0 (
 echo "ERROR!!! Build failed here!" 
 goto end)
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com" projects\json_config_vs14.sln /rebuild "Release|x64" 
-echo %ERRORLEVEL%
+%VS_14_EXE_PATH% projects\json_config_vs14.sln /rebuild "Release|x64" 
+echo "ERRORLEVEL:"%ERRORLEVEL%
 IF NOT %ERRORLEVEL% EQU 0 (
 echo "ERROR!!! Build failed here!" 
 goto end)
