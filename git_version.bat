a27a0d018b8e3aac958357d4c8401344edde0fbf
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-18 14:43:38 +0800

    直接敲git.exe的ERRORLEVEL会不是0，加上--version顺便看下git版本号.

--- git_version.bat
+++ git_version.bat
@@ -3,7 +3,7 @@
 set GIT_COMMAND_PATH="C:\Program Files (x86)\Git\cmd\"
 set PATH=%PATH%;%GIT_COMMAND_PATH%
 
-git.exe
+git.exe --version
 IF NOT %ERRORLEVEL% EQU 0 (
 echo "ERROR!!! Cannot find git.exe, set GIT version to default 0(0000000)" 
 set GIT_VERSION_COMMIT=0
