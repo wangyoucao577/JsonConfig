8055fca793ce658968cafeea23f7ab6180d234bd
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-20 14:53:44 +0800

    新增test项目

---
+++ src/test/main.cc
@@ -0,0 +1,16 @@
+#include "libjson_config/json_config.h"
+
+int main()
+{
+    int jc_major = 0, jc_minor = 0;
+    const char * jc_ver_str = JsonConfig::libjson_config_version(&jc_major, &jc_minor);
+    printf("libjson_config major %d minor %d.\n", jc_major, jc_minor);
+    printf("%s\n", jc_ver_str);
+
+    //wait before return
+    printf("Please press any key to continue...");
+    char a;
+    scanf_s("%c", &a, (int)sizeof(a));
+    return 0;
+    return 0;
+}
